f9c80bfb05ee11d6030b2abded3736661f45cf04
Nawab-AS <59747708+Nawab-AS@users.noreply.github.com>
2025-11-30 21:02:25 -0500

    removed tile smoothening

--- roads.js
+++ roads.js
@@ -35,42 +35,17 @@ export default function loadRoads(...args) {
                 '~': '#0000FF', // water
             };
 
+
             for (let y = 0; y < this.currentMap.length; y++) {
                 for (let x = 0; x < this.currentMap[y].length; x++) {
                     const char = this.currentMap[y][x];
-                    if (!legend[char]) continue;
-                    let points = [];
-                    const tileSize = this.tileSize;
-
-                    const corners = [
-                        { dx: -1, dy: -1, px: 0, py: 0, ex: tileSize/2, ey: 0 }, // top-left
-                        { dx: 1, dy: -1, px: tileSize, py: 0, ex: tileSize, ey: tileSize/2 }, // top-right
-                        { dx: 1, dy: 1, px: tileSize, py: tileSize, ex: tileSize/2, ey: tileSize }, // bottom-right
-                        { dx: -1, dy: 1, px: 0, py: tileSize, ex: tileSize/2, ey: 0 }, // bottom-left
-                    ];
-
-                    for (const { dx, dy, px, py, ex, ey } of corners) {
-                        const nx = x + dx;
-                        const ny = y + dy;
-                        if (
-                            nx < 0 || ny < 0 ||
-                            nx >= this.currentMap[y].length ||
-                            ny >= this.currentMap.length
-                        ) {
-                            points.push(vec2(px, py));
-                        } else if (this.currentMap[ny][nx] == char) {
-                            points.push(vec2(px, py));
-                        }
-
-                        points.push(vec2(ex, ey));
-                    }
-
-                    if (points.length <= 2) continue;
-
+                    const me = legend[char];
+                    if (!me) continue;
+                    let points = []
                     add([
-                        pos(x * tileSize, y * tileSize),
-                        color(legend[char]),
-                        polygon(points),
+                        pos(x * this.tileSize, y * this.tileSize),
+                        color(me),
+                        rect(this.tileSize, this.tileSize),
                     ]);
                 }
             }
